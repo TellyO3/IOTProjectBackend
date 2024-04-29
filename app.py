4cf62f9cda256a55bce76b808a4f512ac03ba50b
tellyo3 <tellyo3gaming@gmail.com>
2024-04-29 14:14:54 +0200

    init

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+global count
+count = 0
+
+
+@app.route("/update", methods=['POST'])
+def update():
+    global count
+
+    print(request.get_json())
+
+    data = request.get_json()
+    count += data['queue_length_update']
+
+    print(count)
+
+    return data, 200
+
 
 if __name__ == '__main__':
     app.run()
