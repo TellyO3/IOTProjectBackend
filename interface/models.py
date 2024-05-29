c104c278cdb96097f3e1b0a5eea69379514d4eb8
Melchior Somers <m.somers@st.hanze.nl>
2024-05-29 21:57:21 +0200

    Refactor import statements in interface/models.py
    With this commit, the import statements have been refactored in the interface/models.py file. The 'login_manager' is now being imported from 'extensions', rather than from '__init__'. This change streamlines import references and improves code clarity.

--- interface/models.py
+++ interface/models.py
@@ -1,5 +1,4 @@
-from __init__ import login_manager
-from extensions import db
+from extensions import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
